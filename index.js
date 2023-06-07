bff71511d6d863939d686894e8aaa6972fc70752
reyesjl <reyesjl.nine@hotmail.com>
2023-06-07 00:56:53 -0400

    initial express commit

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // requirements
-const express= require('express');
-const app = express()
+const path = require('path');
+const express = require('express');
+const app = express();
 const productsRouter = require('./routes/products');
 
 // setup middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// setup view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // routes
 app.use('/api/products', productsRouter);
 
 // start server
-const PORT = 3000
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`It lives on localhost:${PORT}`);
 });
--- routes/products.js
+++ routes/products.js
@@ -3,13 +3,20 @@ const router = express.Router();
 
 // Fetch all products
 router.get('/', (req, res) => {
-    res.send('Get all products');
+    const products = [
+        {id: 1, title: 'fruit', price: '1.50', desc: 'fresh, sweet, vibrant'},
+        {id: 2, title: 'vegetables', price: '2.25', desc: 'hearty, crisp, fresh'},
+        {id: 3, title: 'sweets', price: '0.75', desc: 'sweet, hard, chocolate'}
+    ];
+
+    res.render('products', { products });
 });
 
 // Fetch a single product
 router.get('/:id', (req, res) => {
     const productId = req.params.id;
-    res.send(`Get product with ID ${productId}`)
+
+    res.send(`Get product with ID ${productId}`);
 });
 
 // Create a new product 
@@ -18,7 +25,7 @@ router.post('/', (req, res) => {
     const productPrice = req.params.price;
     const productDesc = req.params.desc;
 
-    res.send('Create new product.')
+    res.send('Create new product.');
 });
 
 // Update a product
@@ -27,13 +34,15 @@ router.put('/:id', (req, res) => {
     const productTitle = req.params.title;
     const productsPrice = req.params.price;
     const productsDesc = req.params.desc;
+
     res.send(`Update product with ID ${productId}`);
 });
 
 // Delete a product
 router.delete(':/id', (req, res) => {
     const productId = req.params.id;
-    res.send(`Delete product with ID ${productId}`)
+
+    res.send(`Delete product with ID ${productId}`);
 });
 
 module.exports = router;

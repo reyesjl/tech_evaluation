e0563d85cc47381d114a63504efaf9c704cd1125
reyesjl <reyesjl.nine@hotmail.com>
2023-06-07 15:05:52 -0400

    db connection file

---
+++ db.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const { Pool } = require('pg');
+
+// configure postgres
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+});
+
+module.exports = pool;
